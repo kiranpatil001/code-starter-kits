c278e904cb8be7c63756096988486a5569d0d86a
Kiran Patil <kiranpatilcse@gmail.com>
2022-01-17 11:56:28 +0530

    Logic implementation completed

--- bowling-ball/BowlingBall.Tests/GameFixture.cs
+++ bowling-ball/BowlingBall.Tests/GameFixture.cs
@@ -6,20 +6,117 @@ namespace BowlingBall.Tests
     [TestClass]
     public class GameFixture
     {
+        IGame game;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            game = new Game();
+        }
+
+        #region Test Methods
+
         [TestMethod]
         public void Gutter_game_score_should_be_zero_test()
         {
-            var game = new Game();
-            Roll(game, 0, 20);
+            Roll(0, 20);
             Assert.AreEqual(0, game.GetScore());
         }
 
-        private void Roll(Game game, int pins, int times)
+        private void Roll(int pins, int times)
         {
             for (int i = 0; i < times; i++)
             {
                 game.Roll(pins);
             }
         }
+        [TestMethod]
+        public void Gutter_game_score_should_be_One_test()
+        {
+            // ACT
+            RollPins(20, 1);
+
+            // ASSERT
+            Assert.AreEqual(20, game.GetScore());
+        }
+
+        [TestMethod]
+        public void Gutter_game_score_should_be_Spare_test()
+        {
+            // ACT
+            RollSpare();
+            game.Roll(3);
+            RollPins(17, 0);
+
+            // ASSERT
+            Assert.AreEqual(16, game.GetScore());
+        }
+
+        [TestMethod]
+        public void Gutter_game_score_should_be_Strike_test()
+        {
+            // ACT
+            RollSpike();
+            game.Roll(3);
+            game.Roll(4);
+            RollPins(16, 0);
+
+            // ASSERT
+            Assert.AreEqual(24, game.GetScore());
+        }
+
+        [TestMethod]
+        public void Gutter_game_score_With_Perfect_Game_test()
+        {
+            // ACT
+            RollPins(12, 10);
+
+            // ASSERT
+            Assert.AreEqual(300, game.GetScore());
+        }
+
+
+        [TestMethod]
+        public void Gutter_game_score_With_Random_Game_test()
+        {
+            // ACT
+            game.Roll(3);
+            game.Roll(5); //8
+            game.Roll(4); //12
+            game.Roll(5); //17
+            game.Roll(2); //19
+            game.Roll(4); //23
+            game.Roll(6); //29
+            game.Roll(1); //30
+            RollSpike(); //40
+
+
+            // ASSERT
+            Assert.AreEqual(40, game.GetScore());
+        }
+
+        #endregion Test Methods
+
+        #region Private Methods
+        private void RollSpike()
+        {
+            game.Roll(10);
+        }
+
+        private void RollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+
+        private void RollPins(int numberOfRolls, int pinHitsPerRoll)
+        {
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                game.Roll(pinHitsPerRoll);
+            }
+        }
+
+        #endregion Private Methods    
     }
 }
--- bowling-ball/BowlingBall/Game.cs
+++ bowling-ball/BowlingBall/Game.cs
@@ -6,17 +6,86 @@ using System.Threading.Tasks;
 
 namespace BowlingBall
 {
-    public class Game
+    public class Game:IGame
     {
+        private List<int> _rolls = new List<int>(21);
+        private int _currentRole = 0;
+        public int Score { get; set; }
+
+        public Game()
+        {
+            for (int i = 0; i < 22; i++)
+            {
+                _rolls.Add(0);
+            }
+        }
+
         public void Roll(int pins)
         {
             // Add your logic here. Add classes as needed.
+            _rolls[_currentRole++] = pins;
         }
 
         public int GetScore()
         {
             // Returns the final score of the game.
-            return 0;
+            int score = 0;
+            int frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    //strike 
+
+                    score += 10 + StrikeBonus(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    //spare
+
+                    score += 10 + SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += NormalFrameBonus(frameIndex);
+                    frameIndex += 2;
+                }
+
+            }
+
+            return score;
         }
+
+        #region private methods
+
+        private int NormalFrameBonus(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1]; ;
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return _rolls[frameIndex + 2];
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return _rolls[frameIndex] == 10;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+        }
+
+        #endregion private methods
     }
 }
